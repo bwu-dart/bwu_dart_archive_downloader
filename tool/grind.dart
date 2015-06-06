c37ce4c6802bc59e6f8ad49dbbb8216e18e225d9
zoechi <gzoechi@gmail.com>
2015-06-06 12:58:17 +0200

    - fix customized test task (won't work until a new Grinder version containing the fix for async tasks is released)

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,9 +1,10 @@
 library bwu_dart_archive_downloader.tool.grind;
 
 export 'package:bwu_utils_dev/grinder/default_tasks.dart' hide main, testWeb;
-import 'package:bwu_utils_dev/grinder/default_tasks.dart' show grind, testTask;
+import 'package:bwu_utils_dev/grinder/default_tasks.dart'
+    show grind, testTask, testTaskImpl;
 
 main(List<String> args) {
-  testTask = () => testTask(['vm']);
+  testTask = ([_]) => testTaskImpl(['vm']);
   grind(args);
 }
