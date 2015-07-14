79e9e33e3d24f47fcfbea4f899338206d3fcd308
zoechi <gzoechi@gmail.com>
2015-07-14 09:24:22 +0200

    - fix grinder tasks to not search for subprojects in the download directory `temp`

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,3 +1,21 @@
 library bwu_dart_archive_downloader.tool.grind;
 
-export 'package:bwu_grinder_tasks/bwu_grinder_tasks.dart';
+import 'dart:io' as io;
+import 'package:path/path.dart' as path;
+import 'package:bwu_grinder_tasks/bwu_grinder_tasks.dart' as tasks;
+export 'package:bwu_grinder_tasks/bwu_grinder_tasks.dart' hide main;
+
+main(List<String> args) {
+  tasks.getSubProjects = getSubProjectsImpl;
+  tasks.main(args);
+}
+
+// Like default but excludes `temp` directory
+List<io.Directory> getSubProjectsImpl() => io.Directory.current
+    .listSync(recursive: true)
+    .where((d) => d.path.endsWith('pubspec.yaml') &&
+        !d.absolute.path.startsWith(
+            path.join(io.Directory.current.absolute.path, 'temp')) &&
+        d.parent.absolute.path != io.Directory.current.absolute.path)
+    .map((d) => d.parent)
+    .toList();
